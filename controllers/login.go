a8c76a1e635510f433c23b3a74799aaea2749e56
rohit <rohit.gharwal910@gmail.com>
2022-08-20 03:49:17 +0530

    call controller

--- controllers/login.go
+++ controllers/login.go
@@ -1,12 +1,10 @@
 package controllers
 
-import (
-  "net/http"
+import "github.com/gin-gonic/gin"
 
-  "github.com/gin-gonic/gin"
-)
 func LoginUser(c *gin.Context) {
-  c.JSON(200, gin.H{
-      "message": "hello world",
-  })
+
+	c.JSON(200, gin.H{
+		"message": "controller call!!",
+	})
 }
--- main.go
+++ main.go
@@ -1,17 +1,27 @@
 package main
-import "github.com/gin-gonic/gin"
-import "github.com/golang/controllers"
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/golang/controllers"
+)
+
 func main() {
-   router := gin.Default()
-   router.GET("/", func(c *gin.Context) {
-       c.JSON(200, gin.H{
-           "message": "Welcome to Go lang!!",
-       })
-   })
-   router.GET("/login", controllers.LoginUser)
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Welcome to Go lang!!",
+		})
+	})
 
+	router.GET("/login", controllers.LoginUser)
 
-
-   
-   router.Run(":9999")
+	router.Run(":9999")
 }
+
+/*
+func LoginUser(c *gin.Context){
+  c.JSON(200, gin.H{
+      "message": "Welcome to Go lang!!",
+  })
+}
+*/
